82233966478bf1bc1ae8a00c428e78b8fe094266
Soufiane Fariss <rev.fariss@gmail.com>
2024-08-09 17:51:49 +0200

    update base url to /capa/

--- web/explorer/vite.config.js
+++ web/explorer/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
     const isBundle = mode === "bundle";
 
     return {
-        base: "/",
+        base: "/capa/",
         plugins: isBundle ? [vue(), viteSingleFile()] : [vue()],
         resolve: {
             alias: {
