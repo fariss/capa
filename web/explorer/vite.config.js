b3d80c156d10389dea2a63eafdb720fed54baba9
Soufiane Fariss <rev.fariss@gmail.com>
2024-08-09 17:32:26 +0200

    set base to /capa/

--- web/explorer/vite.config.js
+++ web/explorer/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
     const isBundle = mode === "bundle";
 
     return {
-        base: "/capa/explorer/",
+        base: "/capa/",
         plugins: isBundle ? [vue(), viteSingleFile()] : [vue()],
         resolve: {
             alias: {
