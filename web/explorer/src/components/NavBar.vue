cd302bed86fce4bbbf25d81134c4fa9c31900100
Soufiane Fariss <rev.fariss@gmail.com>
2024-08-12 16:18:34 +0200

    fix workflow error

--- web/explorer/src/components/NavBar.vue
+++ web/explorer/src/components/NavBar.vue
@@ -18,10 +18,13 @@ const items = ref([
             <div class="flex align-items-center gap-3">
                 <a
                     v-ripple
-                    href="https://github.com/mandiant/capa"
                     class="flex align-items-center justify-content-center text-color w-2rem"
-                >
-                    <i id="gitsub-icon" class="pi pi-github text-2xl"></i>
+                    v-tooltip.bottom="'Download capa Explorer Web for offline usage'"
+                    href="/capa-explorer-web.zip"
+                    download="capa-explorer-web.zip"
+                    aria-label="Download capa Explorer Web release"
+                 >
+                    <i class="pi pi-download text-2xl"></i>
                 </a>
                 <img src="@/assets/images/icon.png" alt="Logo" class="w-2rem" />
             </div>
